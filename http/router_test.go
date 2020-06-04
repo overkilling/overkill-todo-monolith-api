9c8ad541f0366e3b640ebc446548638b63e0aabe
Paulo Schneider <paulo.schneider@gmail.com>
2020-06-04 18:12:29 +0100

    Introduce endpoints to configure the router

--- http/router_test.go
+++ http/router_test.go
@@ -1,70 +1,51 @@
 package http_test
 
 import (
-	"context"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"testing"
 
 	_ "github.com/lib/pq"
 	todoHttp "github.com/overkilling/overkill-todo-monolith-api/http"
-	"github.com/overkilling/overkill-todo-monolith-api/postgres"
-	"github.com/overkilling/overkill-todo-monolith-api/testcontainers"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestIntegrationRouter(t *testing.T) {
-	t.Skip("skipping integration tests.")
-	if testing.Short() {
-		t.Skip("skipping integration tests.")
+func TestRouter(t *testing.T) {
+	config := todoHttp.Endpoints{
+		Healthcheck: testGetHandler("healthcheck"),
+		Todos:       testGetHandler("todos"),
 	}
+	go todoHttp.NewRouter(config).ServeOn(3000)
 
-	ctx := context.Background()
-	container, err := testcontainers.NewPostgresContainer(ctx)
-	if err != nil {
-		panic(err)
+	tt := []struct {
+		endpoint string
+		expected string
+	}{
+		{endpoint: "/health", expected: "healthcheck"},
+		{endpoint: "/todos", expected: "todos"},
 	}
-	defer container.Terminate(ctx)
+	for _, tc := range tt {
+		t.Run(tc.endpoint, func(t *testing.T) {
+			res, err := http.Get(fmt.Sprintf("http://localhost:3000%s", tc.endpoint))
+			if err != nil {
+				panic(err)
+			}
 
-	ip, err := container.Host(ctx)
-	if err != nil {
-		panic(err)
+			content, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				panic(err)
+			}
+
+			assert.Equal(t, tc.expected, string(content))
+
+		})
 	}
-	port, err := container.MappedPort(ctx, "5432")
-	if err != nil {
-		panic(err)
+}
+
+func testGetHandler(response string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, response)
 	}
-
-	db, err := postgres.NewDb(
-		postgres.DbName("todo"),
-		postgres.Credentials("postgres", "postgres"),
-		postgres.HostAndPort(ip, port.Int()),
-		postgres.SslDisabled())
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	err = postgres.MigrateDB(db, "file://../postgres/migrations")
-	if err != nil {
-		panic(err)
-	}
-
-	go todoHttp.NewRouter(db).ServeOn(3000)
-
-	res, err := http.Get("http://localhost:3000/health")
-	if err != nil {
-		panic(err)
-	}
-
-	content, _ := ioutil.ReadAll(res.Body)
-	assert.Equal(t, "{\"status\":\"ok\"}", string(content))
-
-	res, err = http.Get("http://localhost:3000/todos")
-	if err != nil {
-		panic(err)
-	}
-
-	content, _ = ioutil.ReadAll(res.Body)
-	assert.Equal(t, "[{\"todo\":\"Some task\"}]", string(content))
 }
