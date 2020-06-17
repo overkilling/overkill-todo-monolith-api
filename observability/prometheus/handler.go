d3f8fada14c802c1631e81a6aa2aafc27754b54b
Paulo Schneider <paulo.schneider@gmail.com>
2020-06-17 14:43:09 +0100

    Add function to instrument handler with counter, duration and inflight requests

--- observability/prometheus/handler.go
+++ observability/prometheus/handler.go
@@ -1,12 +1,46 @@
 package prometheus
 
 import (
+	"fmt"
 	"net/http"
 
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var instrumentationLabels = []string{"code", "method"}
 
 // Handler wraps Prometheus package handler
 func Handler() http.Handler {
 	return promhttp.Handler()
+}
+
+// Instrument a HTTP handler with counter, duration and inflight metrics.
+func Instrument(label string, handler http.Handler) http.Handler {
+	requestCounter := promauto.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: fmt.Sprintf("%s_handler_requests_total", label),
+			Help: fmt.Sprintf("Total number of %s requests by HTTP code and method", label),
+		},
+		instrumentationLabels,
+	)
+	inFlightGauge := promauto.NewGauge(
+		prometheus.GaugeOpts{
+			Name: fmt.Sprintf("%s_handler_requests_in_flight", label),
+			Help: fmt.Sprintf("Number of %s requests currently in flight", label),
+		},
+	)
+	durationHistogram := promauto.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name: fmt.Sprintf("%s_handler_requests_duration", label),
+			Help: fmt.Sprintf("Histogram of %s request durations in seconds", label),
+		},
+		instrumentationLabels,
+	)
+
+	return promhttp.InstrumentHandlerCounter(requestCounter,
+		promhttp.InstrumentHandlerInFlight(inFlightGauge,
+			promhttp.InstrumentHandlerDuration(durationHistogram,
+				handler)))
 }
--- observability/prometheus/handler_test.go
+++ observability/prometheus/handler_test.go
@@ -10,14 +10,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestHandler(t *testing.T) {
-	res := httptest.NewRecorder()
+var res = httptest.NewRecorder()
+
+func TestInstrumentation(t *testing.T) {
+	testreq, _ := http.NewRequest("GET", "http://localhost:3000/test", nil)
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Test handler"))
+	}
+	prometheus.Instrument("test", http.HandlerFunc(handler)).ServeHTTP(res, testreq)
+
 	req, err := http.NewRequest("GET", "http://localhost:3000/metrics", nil)
 	assert.NoError(t, err, "failed to create request")
-
 	prometheus.Handler().ServeHTTP(res, req)
 
 	content, err := ioutil.ReadAll(res.Body)
 	assert.NoError(t, err, "failed to read response's body")
-	assert.Contains(t, string(content), "promhttp_metric_handler_requests_total")
+	assert.Contains(t, string(content), `test_handler_requests_total{code="200",method="get"} 1`)
+	assert.Contains(t, string(content), `test_handler_requests_in_flight 0`)
+	assert.Contains(t, string(content), `test_handler_requests_duration_count{code="200",method="get"} 1`)
 }
