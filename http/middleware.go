9382ab21bd2b67a2f2758fff1487b669719f49a0
Paulo Schneider <paulo.schneider@gmail.com>
2020-06-18 15:11:33 +0100

    Call panic handler when recovering from panic

--- http/middleware.go
+++ http/middleware.go
@@ -2,13 +2,24 @@ package http
 
 import "net/http"
 
+// RecovererPanicHandler is registered in the Recoverer middleware and
+// is called whenever the middleware recovers from a panic.
+// Useful to log an error message or to report an error metric.
+type RecovererPanicHandler interface {
+	HandlePanic(panicReason interface{})
+}
+
 // Recoverer middleware for capturing panics and delegating to
 // appropriate obsevability handlers. Returns a HTTP 500 status code.
-func Recoverer() func(http.Handler) http.Handler {
+func Recoverer(panicHandlers ...RecovererPanicHandler) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			defer func() {
 				if panicReason := recover(); panicReason != nil {
+					for _, handler := range panicHandlers {
+						handler.HandlePanic(panicReason)
+					}
+
 					w.WriteHeader(http.StatusInternalServerError)
 				}
 			}()
